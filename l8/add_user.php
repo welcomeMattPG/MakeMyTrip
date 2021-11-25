67e218d4c71fff8a4b4d759adcb3867cdd2a83bd
natchapolk <tactiledev.nat@gmail.com>
2021-11-26 00:01:23 +0700

    add natchapol lab8

---
+++ l8/add_user.php
@@ -0,0 +1,110 @@
+<?php require_once('connect.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>CSS326 Sample</title>
+<link rel="stylesheet" href="default.css">
+</head>
+
+<body>
+
+<div id="wrapper"> 
+	<div id="div_header">
+		CSS326 System 
+	</div>
+	<div id="div_subhead">
+		<ul id="menu">
+			<li><a href="user.php">User Profile</a></li>
+			<li><a href="add_user.php">Add User</a></li>
+			<li><a href="group.php">User Group</a></li>
+			<li><a href="add_group.html">Add User Group</a></li>
+		</ul>		
+	</div>
+	<div id="div_main">
+		<div id="div_left">
+				
+		</div>
+		<div id="div_content" class="form">
+			<!--%%%%% Main block %%%%-->
+			<!--Form -->
+			
+			<form action="user.php" method="post">
+					<h2>User Profile</h2>
+					<label>Title</label>
+					<select name="title">
+					<?php
+						$q='select TITLE_ID, TITLE_NAME from TITLE;';
+						if($result=$mysqli->query($q)){
+							while($row=$result->fetch_array()){
+								echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+							}
+						}else{
+							echo 'Query error: '.$mysqli->error;
+						}
+					?>
+					</select>					
+					<label>First name</label>
+					<input type="text" name="firstname">
+						
+					<label>Last name</label>
+					<input type="text" name="lastname">
+
+					<label>Gender</label>
+					<?php
+						$q='select GENDER_ID, GENDER_NAME from GENDER;';
+						if($result=$mysqli->query($q)){
+							while($row=$result->fetch_array()){
+								echo '<input type="radio" name="gender" value="'.$row[0].'">'.$row[1];
+							}
+						}else{
+							echo 'Query error: '.$mysqli->error;
+						}
+					?>
+					<div></div>
+					
+					<label>Email</label>
+					<input type="text" name="email">
+					
+					<h2> Account Profile</h2>
+					<label>Username</label>
+					<input type="text" name="username">
+					
+					<label>Password</label>
+					<input type="password" name="passwd">
+					
+					<label>Confirmed password</label>
+					<input type="password" name="cpasswd">
+					
+					<label>User group</label>
+					<select name="usergroup">
+					<?php
+						$q='select USERGROUP_ID, USERGROUP_NAME from USERGROUP;';
+						if($result=$mysqli->query($q)){
+							while($row=$result->fetch_array()){
+								echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+							}
+						}else{
+							echo 'Query error: '.$mysqli->error;
+						}
+					?>
+					</select>				
+					<label>Disabled</label>
+					<input type="checkbox" name="disabled" value="1">
+					<input type="hidden" name="page" value="adduser" >
+					<div class="center">
+						<input type="submit" name="sub" value="Submit">			
+					</div>
+				</form>
+		</div> <!-- end div_content -->
+		
+	</div> <!-- end div_main -->
+	
+	<div id="div_footer">  
+		
+	</div>
+
+</div>
+</body>
+</html>
+
+
---
+++ l8/editinfo.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CSS326 Sample</title>
+<link rel="stylesheet" href="default.css">
+</head>
+
+<body>
+
+<div id="wrapper"> 
+	<div id="div_header">
+		CSS326 System 
+	</div>
+	<div id="div_subhead">
+		<ul id="menu">
+			<li><a href="user.php">User Profile</a></li>
+			<li><a href="add_user.php">Add User</a></li>
+			<li><a href="group.php">User Group</a></li>
+			<li><a href="add_group.html">Add User Group</a></li>
+		</ul>		
+	</div>
+	<div id="div_main">
+		<div id="div_left">
+				
+		</div>
+		<div id="div_content" class="form">
+		<h2>Edit User Group</h2>
+		<?php
+		 $uid = $_GET['uid'];
+		require_once('connect.php'); 
+		$q="SELECT * FROM USERGROUP where USERGROUP_ID=$uid";
+		 $result = $mysqli->query($q);
+			echo "<form action='updateg.php' method='post'>";
+			while($row=$result->fetch_array()){
+			echo "<label>Group Code</label>";
+			echo "<input type=text name=groupcode value=".$row['USERGROUP_CODE']."><br>";
+			
+			echo "<label>Group Name</label>";
+			echo "<input type=text name=groupname value=".$row['USERGROUP_NAME']."><br>";
+			
+			echo "<label>Remark</label>";
+			echo "<textarea name=remark>".$row['USERGROUP_REMARK']."</textarea><br>";
+			echo "<input type=hidden name=uid value='".$row['USERGROUP_ID']."'><br>";
+			
+			echo "<label>Url</label>";
+			echo "<input type=text name=groupurl value=".$row['USERGROUP_URL']."><br>";
+			echo "<div class='center'>";			
+			echo "<input type=submit name=su value=submit>";
+			echo "<input type='reset' value='Cancel'>	";
+			echo "</div>";
+			}
+			echo "</form>";
+			
+		?>
+			
+		</div> <!-- end div_content -->
+		
+	</div> <!-- end div_main -->
+	
+	<div id="div_footer">  
+		
+	</div>
+
+</div>
+</body>
+</html>
+
